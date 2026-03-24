c51d71a78cc3d7bccd01b7699148fceb2a0e1d39
zhukockijmaxim9 <zhukockijmaxim@gmail.com>
2026-03-24 23:27:59 +0400

    Добавление возможности отдельно запускать фронт, так же запускать вместе фронт и бек одной командой

--- frontend/js/bootstrap.js
+++ frontend/js/bootstrap.js
@@ -2,3 +2,5 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.withCredentials = true;
+window.axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    server: {
+        open: '/frontend/index.html',
+    },
     plugins: [
         laravel({
             input: ['frontend/scss/app.scss', 'frontend/js/index.jsx'],
